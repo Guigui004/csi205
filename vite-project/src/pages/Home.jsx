e2dfdd583af6530f47f2591ad2d880e738a80c46
Guigui004 <burachai.sal@spumail.net>
2025-11-13 21:12:44 +0700

    initial commit

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <div className=" home-container ">
+      <div className="profile-pic"></div>
+        <p className="home-text" >
+          <strong >นาย บุราชัย สละสำราญ</strong><br />
+          รหัสนักศึกษา: 67172354 <br />
+          ชั้นปีที่ 2 สาขาวิทยาการคอมพิวเตอร์และนวัตกรรมการพัฒนาซอฟต์แวร์<br />
+          คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยศรีปทุม<br />
+          ชอบเล่นเกมหลายๆเเนว และพักผ่อน
+        </p>
+
+        
+      
+    </div>
+  );
+};
+
+export default Home;
